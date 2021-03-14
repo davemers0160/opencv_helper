a4244e3e94dc3face58a4c807feaac384f334633
Dave <davemers@iu.edu>
2021-03-14 12:58:43 -0400

    adding custom way to select roi instead of builtin opencv function

--- select_roi_lib/src/select_roi.cpp
+++ select_roi_lib/src/select_roi.cpp
@@ -15,14 +15,54 @@
 //----------------------------------------------------------------------------------
 // library internal state variables:
 
+
 //----------------------------------------------------------------------------------
-void select_roi(char* win_name,
+typedef struct mouse_params
+{
+    bool is_drawing = false;
+    cv::Rect roi;
+
+} mouse_params;
+
+
+// ----------------------------------------------------------------------------------------
+void mouse_click(int event, int x, int y, int flags, void* param)
+{
+    mouse_params* mp = (mouse_params*)param;
+
+    switch (event)
+    {
+    case cv::EVENT_LBUTTONDOWN:
+        (*mp).is_drawing = true;
+        (*mp).roi = cv::Rect(x, y, 0, 0);
+        break;
+
+    case cv::EVENT_MOUSEMOVE:
+        if ((*mp).is_drawing)
+        {
+            (*mp).roi = cv::Rect(std::min((*mp).roi.x, x), std::min((*mp).roi.y, y), std::abs(x - (*mp).roi.x), std::abs(y - (*mp).roi.y));
+        }
+        break;
+
+    case cv::EVENT_LBUTTONUP:
+        (*mp).roi = cv::Rect(std::min((*mp).roi.x, x), std::min((*mp).roi.y, y), std::abs(x - (*mp).roi.x), std::abs(y - (*mp).roi.y));
+        (*mp).is_drawing = false;
+        break;
+    }
+
+}   // end of mouse_click
+
+//----------------------------------------------------------------------------------
+void select_roi(
     unsigned char* src, 
     unsigned int w, unsigned int h, unsigned int c,
-    int *rx, int *ry, int *rw, int *rh)
+    int *rx, int *ry, int *rw, int *rh
+)
 {
-    cv::Mat img;
+    std::string win_name = "Select ROI";
 
+    cv::Mat img;
+    
     switch(c)
     {
     case 1:
@@ -35,12 +75,38 @@ void select_roi(char* win_name,
     
     cv::Mat img2 = img.clone();
 
-    cv::Rect roi = cv::selectROI(std::string(win_name), img2);
-    //cv::destroyWindow(std::string(win_name));
+    mouse_params mp;
+
+    cv::namedWindow(win_name, cv::WINDOW_NORMAL);
+
+    cv::setMouseCallback(win_name, mouse_click, (void*)&mp);
+
+    char key = 0;
+
+    // end selection process on SPACE (32) ESC (27) or ENTER (13)
+    while (!(key == 32 || key == 27 || key == 13))
+    {
+        cv::rectangle(img2, mp.roi, cv::Scalar(255, 0, 0), 2, 1);
+
+        cv::imshow(win_name, img2);
+
+        img2 = img.clone();
+
+        key = cv::waitKey(10);
+    }
+
+    cv::destroyWindow(win_name);
+
+    //cv::Rect roi = cv::selectROI(std::string(win_name), img2);
     
-    *rx = roi.x;
-    *ry = roi.y;
-    *rw = roi.width;
-    *rh = roi.height;  
+    //cv::waitKey(1);
+
+    //cv::destroyWindow(std::string(win_name));
+    //cv::destroyAllWindows();
+
+    *rx = mp.roi.x;
+    *ry = mp.roi.y;
+    *rw = mp.roi.width;
+    *rh = mp.roi.height;
 
 }   // end of select_roi
