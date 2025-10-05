20639c62e85ce8c0ed7813b8a0fdd45c660baf78
david emerson <davemers0160@gmail.com>
2025-10-05 15:02:14 -0400

    Update draw_line.cpp

--- draw_line/src/draw_line.cpp
+++ draw_line/src/draw_line.cpp
@@ -9,101 +9,130 @@
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
 
-cv::Point start_point;
-cv::Point stop_point;
-bool drawing = false;
-cv::Mat original_image;
-cv::Mat current_image;
+//cv::Point start_point;
+//cv::Point stop_point;
+//bool drawing = false;
+//cv::Mat original_image;
+//cv::Mat current_image;
 
-int font_face = cv::FONT_HERSHEY_PLAIN;
-int thickness = 1;
-int baseline = 0;
-double font_scale = 1.0;
-cv::Size text_size;
+//int font_face = cv::FONT_HERSHEY_PLAIN;
+//int thickness = 1;
+//int baseline = 0;
+//double font_scale = 1.0;
 
-void onMouse(int event, int x, int y, int flags, void* userdata) 
+typedef struct line_struct
 {
-    if (event == cv::EVENT_LBUTTONDOWN) 
-    {
-        start_point = cv::Point(x, y);
-        drawing = true;
-    } 
-    else if (event == cv::EVENT_MOUSEMOVE) 
-    {
-        if (drawing) 
-        {
-            stop_point = cv::Point(x, y);
-            // Copy original image to temp for real-time drawing preview
-            original_image.copyTo(current_image);
-            cv::line(current_image, start_point, stop_point, cv::Scalar(0, 0, 255), 2); // Red line, thickness 2
-            cv::imshow("Image Window", current_image);
-        }
-    } 
-    else if (event == cv::EVENT_LBUTTONUP) 
-    {
-        drawing = false;
-        stop_point = cv::Point(x, y);
-        cv::line(original_image, start_point, stop_point, cv::Scalar(0, 0, 255), 2); // Draw on original image
-        cv::imshow("Image Window", original_image);
-    }
-}
+    cv::Point start_point;
+    cv::Point stop_point;
+    bool drawing = false;
+    cv::Mat original_image;
+    cv::Mat current_image;
+    cv::Size text_size;
+} line_struct;
+
+//void onMouse(int event, int x, int y, int flags, void* userdata) 
+//{
+//
+//    line_struct *ls = (line_struct *)(userdata);
+//
+//    if (event == cv::EVENT_LBUTTONDOWN) 
+//    {
+//        ls->start_point = cv::Point(x, y);
+//        ls->drawing = true;
+//    } 
+//    else if (event == cv::EVENT_MOUSEMOVE) 
+//    {
+//        if (ls->drawing) 
+//        {
+//            ls->stop_point = cv::Point(x, y);
+//            // Copy original image to temp for real-time drawing preview
+//            ls->original_image.copyTo(ls->current_image);
+//            cv::line(ls->current_image, ls->start_point, ls->stop_point, cv::Scalar(0, 0, 255), 2); // Red line, thickness 2
+//            cv::imshow("Image Window", ls->current_image);
+//        }
+//    } 
+//    else if (event == cv::EVENT_LBUTTONUP) 
+//    {
+//        ls->drawing = false;
+//        ls->stop_point = cv::Point(x, y);
+//        cv::line(ls->original_image, ls->start_point, ls->stop_point, cv::Scalar(0, 0, 255), 2); // Draw on original image
+//        cv::imshow("Image Window", ls->original_image);
+//    }
+//}
 
 // Mouse callback function
 void draw_line_callback(int event, int x, int y, int flags, void* userdata) 
 {
+    const int32_t font_face = cv::FONT_HERSHEY_PLAIN;
+    const int32_t thickness = 1;
+    int32_t baseline = 0;
+    const double font_scale = 1.0;
+
+    line_struct* ls = (line_struct*)(userdata);
+
     if (event == cv::EVENT_LBUTTONDOWN) 
     {
         // A left-click starts a new line
-        drawing = true;
-        start_point = cv::Point(x, y);
+        ls->drawing = true;
+        ls->start_point = cv::Point(x, y);
     }
-    else if (event == cv::EVENT_MOUSEMOVE && drawing) 
+    else if (event == cv::EVENT_MOUSEMOVE && ls->drawing)
     {
-        stop_point = cv::Point(x, y);
+        ls->stop_point = cv::Point(x, y);
 
         // While the mouse is moving and drawing is active
         // 1. Copy the original image to clear the old line
-        original_image.copyTo(current_image);
+        ls->original_image.copyTo(ls->current_image);
 
         // 2. Draw the new line on the temporary image
-        cv::line(current_image, start_point, stop_point, cv::Scalar(0, 0, 255), 2);
+        cv::line(ls->current_image, ls->start_point, ls->stop_point, cv::Scalar(0, 0, 255), 2);
 
-        std::string height_str = "  " + std::to_string(stop_point.y - start_point.y) + "px";
-        text_size = cv::getTextSize(height_str, font_face, font_scale, thickness, &baseline);
+        std::string height_str = "  " + std::to_string(ls->stop_point.y - ls->start_point.y) + "px";
+        ls->text_size = cv::getTextSize(height_str, font_face, font_scale, thickness, &baseline);
 
-        cv::putText(current_image, height_str, stop_point + cv::Point(0, (text_size.height + 3)), font_face, font_scale, cv::Scalar(0, 0, 255), thickness, cv::LINE_8, false);
+        cv::putText(ls->current_image, height_str, ls->stop_point + cv::Point(0, (ls->text_size.height + 3)), font_face, font_scale, cv::Scalar(0, 0, 255), thickness, cv::LINE_8, false);
 
         // 3. Display the updated image
-        cv::imshow("Image Window", current_image);
+        cv::imshow("Image Window", ls->current_image);
     }
     else if (event == cv::EVENT_LBUTTONUP) 
     {
         // A left-button release finishes the line
-        drawing = false;
+        ls->drawing = false;
         // The line is already drawn on current_image from the last mouse move event
     }
 }
 
 int main() 
 {
-    original_image = cv::Mat::zeros(500, 500, CV_8UC3); // Create a black image
-    cv::namedWindow("Image Window", cv::WINDOW_AUTOSIZE);
-    //cv::setMouseCallback("Image Window", draw_line_callback);
+
+    line_struct ls;
+    ls.original_image = cv::Mat::zeros(500, 500, CV_8UC3); // Create a black image
+    //cv::namedWindow("Image Window", cv::WINDOW_AUTOSIZE);
+    //cv::setMouseCallback("Image Window", draw_line_callback);cv::WINDOW_GUI_NORMAL
+    cv::namedWindow("Image Window", cv::WINDOW_OPENGL | cv::WINDOW_AUTOSIZE);
+    //cv::setWindowProperty("Image Window", cv::WND_PROP_ASPECT_RATIO, cv::WINDOW_KEEPRATIO);
 
     // Create a mutable copy of the original image
-    original_image.copyTo(current_image);
+    ls.original_image.copyTo(ls.current_image);
 
     // Set the mouse callback function for the window
-    cv::setMouseCallback("Image Window", draw_line_callback);
+    cv::setMouseCallback("Image Window", draw_line_callback, (void*)&ls);
 
     // Display the initial image
-    cv::imshow("Image Window", current_image);
+    cv::imshow("Image Window", ls.current_image);
 
     // Wait for a key press and close the window
-    cv::waitKey(0);
+    unsigned char key = 0;
+
+    while (key != 27)
+    {
+        key = cv::waitKey(50);
+    }
+    //cv::waitKey(0);
     cv::destroyAllWindows();
 
-    uint32_t height = abs(stop_point.y - start_point.y);
+    uint32_t height = abs(ls.stop_point.y - ls.start_point.y);
 
     std::cout << "height: " << height << std::endl;
 
